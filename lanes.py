a3d60708c165d97d3b07a1071163b6f94d2f2ed0
v47shah <84160951+v47shah@users.noreply.github.com>
2022-08-28 18:30:57 +0530

    Removed commented code

--- lanes.py
+++ lanes.py
@@ -2,16 +2,20 @@ import cv2
 import numpy as np
 
 
-def make_coordinates(image, line_parameters):   # to provide coordinates to the previously specified average slope and av. intercepts
+def make_coordinates(image, line_parameters):
+    # This function provides coordinates to the previously specified average slope and av. intercepts
     slope, intercept = line_parameters
     y1 = image.shape[0]
     y2 = int(y1*3/5)
-    x1 = int((y1 - intercept)/slope)  #Since y = mx+b is our lin equation
+    x1 = int((y1 - intercept)/slope)  #Since y = mx+b is our line equation
     x2 = int((y2 - intercept)/slope)
-    return np.array([x1, y1, x2, y2])
+    return np.array([x1, y1, x2, y2]) #returning array or coordinates
 
 
 def average_slope_intercept(image, lines):
+    # This function calculates the average slopes of the left and right lines in
+    # the lanes and uses the make_coordinates function to get the x,y coordinates
+    # of the line in order to display the lines
     left_fit = []
     right_fit = []
     for line in lines:
@@ -36,7 +40,8 @@ def canny(image):
     canny = cv2.Canny(blur, 50, 150)
     return canny
 
-def display_lines(image, lines):   # to display lines on black image
+def display_lines(image, lines):
+    # This function helps display lines on black image
     line_image = np.zeros_like(image)
     if lines is not None:
         for x1, y1, x2, y2 in lines:
@@ -52,35 +57,12 @@ def region_of_interest(image):
     # applying mask to our generated triangle (applying our desired image onto a black mask)
 
     masked_image = cv2.bitwise_and(image, mask)
-    # ( Computing the bitwise & of both images as we saw earlier in the theory
-    # section, takes the & function of each homologous picture in both arrays,
-    # ultimately masking the canny image to only show the region of interest traced
-    # by the polygon contour of the mask)
+    # to compute the bitwise & of both images takes the & function of each homologous
+    # picture in both arrays, ultimately masking the canny image to only show
+    # the region of interest traced by the polygon contour of the mask)
 
     return masked_image
 
-
-#*******************************************************************************
-# Code to detect lanes for a single image provided (commented out):
-#*******************************************************************************
-
-# image = cv2.imread('test_image.jpg')
-# lane_image = np.copy(image)
-# canny_image = canny(lane_image)
-# cropped_image = region_of_interest(canny_image)
-# lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength = 40, maxLineGap = 5)
-
-#-----#  Note: The smaller the 2nd and 3rd arguents, the more precise the line detector will be and vice-versa.
-#-----#  The 4th argument is the threshold, which signifies the minimum number of points
-#-----#  that have to be alligned in a single straight line for it to be considered in the Hoiugh mapping
-
-# averaged_lines = average_slope_intercept(lane_image, lines)
-# line_image = display_lines(lane_image, averaged_lines)
-# combo_image = addWeighted(lane_image, 0.8, line_image, 1, 1)
-# cv2.imshow('result', combo_image)
-# cv2.waitKey(0)
-
-#*******************************************************************************
 
 
 #*******************************************************************************
@@ -95,7 +77,7 @@ while (cap.isOpened()):
     lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength = 40, maxLineGap = 5)
      #  Note: The smaller the 2nd and 3rd arguents, the more precise the line detector will be and vice-versa.
      #  The 4th argument is the threshold, which signifies the minimum number of points
-     #  that have to be alligned in a single straight line for it to be considered in the Hoiugh mapping
+     #  that have to be alligned in a single straight line for it to be considered in the Hough mapping
 
     averaged_lines = average_slope_intercept(frame, lines)
     line_image = display_lines(frame, averaged_lines)
